802e8555b6806e3387f4e2f845d40c28d21201df
ThiwankaReiss <thiwankar2003@gmail.com>
2024-03-19 18:03:24 +0530

    Initialised country project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+function App() {
+
+
+  return (
+    <div>
+      <p>Hi</p>
+      <button className='btn btn-primary'>c</button>
+    </div>
+  )
+}
+
+export default App
